4d7ff14c115efe51f7f76fcb75689e84455bf637
liuderchi <liuderchi@gmail.com>
2018-10-17 10:40:18 +0800

    WIP test padding space

--- my-plugins/real-cpu-usage.5s.sh
+++ my-plugins/real-cpu-usage.5s.sh
@@ -15,8 +15,13 @@ fi
 
 IDLE=$(top -F -R -l3 | grep "CPU usage" | tail -1 | egrep -o '[0-9]{0,3}\.[0-9]{0,2}% idle' | sed 's/% idle//')
 
-USED=$(echo 100 - "$IDLE" | bc | xargs printf "%.*f\n" 0)
+USED=$(echo 100 - "$IDLE" | bc | xargs printf "%.f\n")
+chrlen=${#USED}
 
-echo "$USED%"
+if [ "${#USED}" == 1 ]; then
+    echo " $USED %"
+else
+    echo "$USED %"
+fi
 echo "---"
 echo "Open Activity Monitor| bash='$0' param1=activitymonitor terminal=false"
